3fc0458a77a98f42b39289df4ca84b00682b7065
Michal Zuber <info@nevilleweb.sk>
2017-08-13 08:22:16 +0200

    Build without uglifyify, with and without debug

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,11 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
-shell.exec("browserify src/bundle.js -o dist/bundle.js -t [ babelify --presets [ es2015 ] ] -v -d -g uglifyify");
-shell.exec("browserify src/bundle2.js -o dist/bundle2.js -t [ babelify --presets [ es2015 ] ] -v -d -g uglifyify");
+
+shell.exec("browserify src/bundle.js -o dist/bundle.js -t [ babelify --presets [ es2015 ] ]");
+shell.exec("browserify src/bundle2.js -o dist/bundle2.js -t [ babelify --presets [ es2015 ] ]");
+
+shell.exec("browserify src/bundle.js -o dist/bundle.min.js -t [ babelify --presets [ es2015 ] ] -g uglifyify");
+shell.exec("browserify src/bundle2.js -o dist/bundle2.min.js -t [ babelify --presets [ es2015 ] ] -g uglifyify");
+
+shell.exec("browserify src/bundle.js -o dist/bundle-debug.min.js -t [ babelify --presets [ es2015 ] ] -v -d -g uglifyify");
+shell.exec("browserify src/bundle2.js -o dist/bundle2-debug.min.js -t [ babelify --presets [ es2015 ] ] -v -d -g uglifyify");
