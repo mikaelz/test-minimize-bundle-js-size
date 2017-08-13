2d40b0e8a731613c614b9af8b0890eac8d13154f
Michal Zuber <info@nevilleweb.sk>
2017-08-13 08:29:17 +0200

    Build with and without source map

--- bin/build.js
+++ bin/build.js
@@ -4,8 +4,11 @@ var shell = require("shelljs");
 shell.exec("browserify src/bundle.js -o dist/bundle.js -t [ babelify --presets [ es2015 ] ]");
 shell.exec("browserify src/bundle2.js -o dist/bundle2.js -t [ babelify --presets [ es2015 ] ]");
 
+shell.exec("browserify src/bundle.js -o dist/bundle-sourcemap.js -t [ babelify --presets [ es2015 ] ] -d");
+shell.exec("browserify src/bundle2.js -o dist/bundle2-sourcemap.js -t [ babelify --presets [ es2015 ] ] -d");
+
 shell.exec("browserify src/bundle.js -o dist/bundle.min.js -t [ babelify --presets [ es2015 ] ] -g uglifyify");
 shell.exec("browserify src/bundle2.js -o dist/bundle2.min.js -t [ babelify --presets [ es2015 ] ] -g uglifyify");
 
-shell.exec("browserify src/bundle.js -o dist/bundle-debug.min.js -t [ babelify --presets [ es2015 ] ] -v -d -g uglifyify");
-shell.exec("browserify src/bundle2.js -o dist/bundle2-debug.min.js -t [ babelify --presets [ es2015 ] ] -v -d -g uglifyify");
+shell.exec("browserify src/bundle.js -o dist/bundle-sourcemap.min.js -t [ babelify --presets [ es2015 ] ] -d -g uglifyify");
+shell.exec("browserify src/bundle2.js -o dist/bundle2-sourcemap.min.js -t [ babelify --presets [ es2015 ] ] -d -g uglifyify");
